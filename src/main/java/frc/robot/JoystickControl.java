a01300fd2960bbdb639ae2d2f4a2368c0a4413ae
JordanLaGroneL9 <59583801+JordanLaGroneL9@users.noreply.github.com>
2020-02-17 10:06:55 -0600

    added a toggle switch to shroud

--- src/main/java/frc/robot/JoystickControl.java
+++ src/main/java/frc/robot/JoystickControl.java
@@ -93,7 +93,7 @@ public class JoystickControl {
              cellShooter.ShootAlign();
 
          }
-         if (driver.getRawButtonReleased(Xbox.BUTTON_RB)) {
+         if (driver.getRawButtonPressed(Xbox.BUTTON_RB)) {
              cellShooter.ShootNoAlign();
          }
         if (gunner.getRawButtonPressed(Xbox.AXIS_LT)){
