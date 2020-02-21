7677d812cc17c73228b10d6181c3759c6715e4b6
EvanJordan13 <ejordan1305@gmail.com>
2020-02-20 21:09:18 -0600

    semi-working swerve

--- src/main/java/frc/robot/MotorFactory.java
+++ src/main/java/frc/robot/MotorFactory.java
@@ -4,6 +4,8 @@ import com.ctre.phoenix.motorcontrol.NeutralMode;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.revrobotics.CANSparkMax.IdleMode;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 public class MotorFactory {
 
     public static enum MotorType {
@@ -27,6 +29,9 @@ public class MotorFactory {
  }
     public static Motor createRotationMotor(int deviceID, int analogEncoderID) {//TODO add an analog encoder
         if (rotationMotorType == MotorType.TALON) {
+            boolean isDevIDLegal = deviceID > 14;
+            
+            SmartDashboard.putBoolean("isDevIDLegal", isDevIDLegal);
             return new TalonFXFalcon(deviceID, NeutralMode.Brake, false, analogEncoderID);
 
         } else if (tractionMotorType == MotorType.NEO) {
